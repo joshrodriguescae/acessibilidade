8db387656ff53d258910142728d5f7a728caf0fd
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2025-02-12 18:23:23 -0300

    Add files via upload

---
+++ def_visao/visao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deficiência Visual</title>
+</head>
+<body>
+    <div>
+    <h1>Principais tipos de Deficiência Visual</h1>
+    <br>
+    <h2>Baixa visão</h2>
+    <br>
+    <h2>Próximo à cegueira</h2>
+    <br>
+    <h2>Cegueira</h2>
+    <br>
+
+
+
+    </div>
+    
+</body>
+</html>
